e713309330e09b95b60a6f04a99d9b13369dfe2d
Mohosin212601~ <mohosin2126@gmail.com>
2023-10-08 04:20:27 +0800

    added rightnav

--- src/Pages/shared/RightNav.jsx
+++ src/Pages/shared/RightNav.jsx
@@ -1,8 +1,45 @@
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from 'react-icons/fa';
+import qzone1 from "../../assets/qZone1.png"
+import qzone2 from "../../assets/qZone2.png"
+import qzone3 from "../../assets/qZone3.png"
+
 
 const RightNav = () => {
     return (
         <div>
-           <h1>hello this is me right nav</h1> 
+            <div className='p-4 space-y-3 mb-6'>
+                <h2 className="text-3xl">Login With</h2>
+                <button className="btn btn-outline w-full">
+                    <FaGoogle></FaGoogle>
+                    Google
+                </button>
+                <button className="btn btn-outline w-full">
+                    <FaGithub></FaGithub>
+                    Github
+                </button>
+            </div>
+            <div className='p-4 mb-6'>
+                <h2 className="text-3xl mb-4">Find Us on</h2>
+                <a className='p-4 flex text-lg items-center border rounded-t-lg' href="">
+                    <FaFacebook className='mr-3'></FaFacebook>
+                    <span>Facebook</span>
+                </a>
+                <a className='p-4 flex text-lg items-center border-x' href="">
+                    <FaTwitter className='mr-2'></FaTwitter>
+                    <span>Twitter</span>
+                </a>
+                <a className='p-4 flex text-lg items-center border rounded-b-lg' href="">
+                    <FaInstagram className='mr-2'></FaInstagram>
+                    <span>Facebook</span>
+                </a>
+            </div>
+          
+            <div className='p-4 space-y-3 mb-6'>
+                <h2 className="text-3xl">Q Zone</h2>
+                <img src={qzone1} alt="" />
+                <img src={qzone2} alt="" />
+                <img src={qzone3} alt="" />
+            </div>
         </div>
     );
 };
