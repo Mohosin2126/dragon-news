65cd9141c73b6e5a63a2982f6d8dd141a24f9563
Mohosin212601~ <mohosin2126@gmail.com>
2023-10-08 16:05:39 +0800

    added few text alignments

--- src/Pages/shared/Login.jsx
+++ src/Pages/shared/Login.jsx
@@ -7,17 +7,17 @@ const Login = () => {
         <div>
             <Navbar></Navbar>
             <div>
-                <h2 className="text-3xl my-10 text-center">Please Login</h2>
+                <h2 className="text-3xl my-10 text-center font-semibold">Please Login</h2>
                 <form className=" md:w-3/4 lg:w-1/2 mx-auto">
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Email</span>
+                            <span className="label-text text-lg ">Email</span>
                         </label>
                         <input type="email" required name="email" placeholder="Email" className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Password</span>
+                            <span className="label-text text-lg">Password</span>
                         </label>
                         <input type="password" required name="password" placeholder="Password" className="input input-bordered" />
                         <label className="label">
@@ -25,7 +25,7 @@ const Login = () => {
                         </label>
                     </div>
                     <div className="form-control mt-6">
-                        <button className="btn btn-primary">Login</button>
+                        <button className="btn btn-neutral">Login</button>
                     </div>
                 </form>
                 <p className="text-center mt-4">Do not have an account <Link className="text-blue-600 font-bold" to="/register">Register</Link></p>
